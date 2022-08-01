c987e2ce4e509b2c77d124e4cb7f321a398bbb53
Estelle <estellegregoire0689@gmail.com>
2022-08-01 17:08:23 +0200

    updates

---
+++ src/EventSubscriber/AdminSubscriber.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Model\TimestampedInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BeforeEntityPersistedEvent::class => ['setEntityCreatedAt'],
+            BeforeEntityUpdatedEvent::class => ['setEntityUpdatedAt'],
+        ];
+    }
